2e4360610742deaa8a88aecebaaf71aa33e8d244
Andrew Real <areal@blazeconcepts.co.uk>
2019-07-17 10:03:13 +0100

    Mini cart update improved theme cross compatibility.

--- js/eventwoo.js
+++ js/eventwoo.js
@@ -10,14 +10,17 @@ function triggerCartReload(){
             woocommerce_params.ajax_url,
             {'action': 'eventwoo_update_mini_cart'},
             function(response) {
-                // TODO : Need to find css classes that work across all themes - this works on Arts Taunton but not on StoreFront
-                jQuery('.mini_cart').html(response);
-                // jQuery('.woocommerce-mini-cart').html(response);
+                // Using the child as selector to avoid problems with the mini-cart changing for some themes.
+                var miniCartChildSelector = '.woocommerce-mini-cart';
+                if ( jQuery('.woocommerce-mini-cart__empty-message').length ){
+                    miniCartChildSelector = '.woocommerce-mini-cart__empty-message';
+                }
+                jQuery(miniCartChildSelector).parent().html(response);
                 setTimeout(function(){
                     // Update the price next to the basket button
-                    const priceHTML = jQuery('.mini_cart .woocommerce-Price-amount').html();
-                    jQuery('.cart-customlocation .woocommerce-Price-amount').html(priceHTML);
-                    jQuery('.cart-contents .woocommerce-Price-amount').html(priceHTML);
+                    const priceHTML = jQuery(miniCartChildSelector).find('.woocommerce-Price-amount').html();
+                    jQuery('.woocommerce-Price-amount').html(priceHTML);
+                    jQuery('.woocommerce-Price-amount').html(priceHTML);
                 }, 100);
             }
         );
